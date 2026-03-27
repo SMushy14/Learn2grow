7fad9b4a3b1337c6fae0562f3871641c17212435
SMushy14 <s.mushy1@alustudemt.com>
2026-03-27 13:42:04 +0200

    terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  backend "s3" {
+    bucket         = "learn2grow-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "af-south-1"
+    use_lockfile   = true
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source       = "./modules/vpc"
+  project_name = var.project_name
+  environment  = var.environment
+  vpc_cidr     = var.vpc_cidr
+}
+
+module "security_groups" {
+  source       = "./modules/security_groups"
+  project_name = var.project_name
+  environment  = var.environment
+  vpc_id       = module.vpc.vpc_id
+}
+
+module "bastion" {
+  source            = "./modules/bastion"
+  project_name      = var.project_name
+  environment       = var.environment
+  public_subnet_id  = module.vpc.public_subnet_ids[0]
+  security_group_id = module.security_groups.bastion_sg_id
+  key_pair_name     = var.key_pair_name
+}
+
+module "ec2" {
+  source            = "./modules/ec2"
+  project_name      = var.project_name
+  environment       = var.environment
+  private_subnet_id = module.vpc.private_subnet_ids[0]
+  security_group_id = module.security_groups.app_sg_id
+  key_pair_name     = var.key_pair_name
+}
+
+module "ecr" {
+  source       = "./modules/ecr"
+  project_name = var.project_name
+  environment  = var.environment
+}
